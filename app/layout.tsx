987bebb62ef3f08c27cb723876ce493518550855
Moris Mwai <mwaimoris@gmail.com>
2025-05-29 22:29:31 +0300

    Implement authentication layout and forms, add Tailwind CSS theme variables, and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${monaSans.className} antialiasing`}
+        className={`${monaSans.className} antialiasing pattern`}
       >
         {children}
       </body>
