42eb694e2be8a71e07beb96a2f706a6be282786d
Moris Mwai <mwaimoris@gmail.com>
2025-06-06 23:59:51 +0300

    Add DisplayTechIcons component and integrate into InterviewCard; enhance layout with role, date, and feedback display

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import dayjs from 'dayjs'
 import Image from 'next/image';
 import { getRandomInterviewCover } from '@/lib/utils';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import DisplayTechIcons from './DisplayTechIcons';
 
 const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
     const feedback = null as Feedback | null;
@@ -17,6 +20,37 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt}:
                 </div>
 
                 <Image src={getRandomInterviewCover()} alt='cover image' width={90} height={90} className='rounded-full object-fit size-[90px]' />
+
+                <h3 className='mt-5 capitalize'>
+                    {role} Interview
+                </h3>
+
+                <div className='flex flex-row gap-5 mt-3'>
+                    <div className='flex flex-row gap-2'>
+                        <Image src='/calendar.svg' alt='calendar' width={22} height={22} />
+                        <p>{formattedDate}</p>
+                    </div>
+
+                    <div className='flex flex-row gap-2 items-center'>
+                        <Image src='/star.svg' alt='star' width={22} height={22} />
+                        <p>{feedback?.totalScore || '---'}/100</p>
+                    </div>
+                </div>
+
+                <p className='line-clamp-2 mt-5'>{feedback?.finalAssessment || "You haven't started your interview journey yet! Start now to improve your skills"}</p>
+            </div>
+
+            <div className='flex flex-row justify-between'>
+                <DisplayTechIcons techStack={techstack} />
+
+                <Button className='btn-primary'>
+                    <Link href={feedback 
+                        ? `/interview/${interviewId}/feedback`
+                        : `/interview/${interviewId}`
+                    }>
+                        {feedback ? 'View Feedback' : 'View Interview'}
+                    </Link>
+                </Button>
             </div>
         </div>
     </div>
